1a229e1a0683d622dcceb8e914cb8d97170354fb
bertRC <sithalbert@ya.ru>
2019-10-21 20:30:07 +0300

    Создан объект-сервис и объекты-товары

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,11 +1,14 @@
 package ru.itpark;
 
-import ru.itpark.model.AbstractProduct;
 import ru.itpark.model.Computer;
+import ru.itpark.model.Smartphone;
+import ru.itpark.repository.ProductRepository;
+import ru.itpark.service.ProductService;
 
 public class Main {
     public static void main(String[] args) {
-        AbstractProduct product = new Computer(1, "Компьютер", 1_000, 4.9, "i3", 4, "HDD 1ТБ");
-        System.out.println(product);
+        final ProductService service = new ProductService(new ProductRepository());
+        service.add(new Smartphone("Смартфон Redmi Note 7", 13_990, 4.0, "blue", 6.3, 32));
+        service.add(new Computer("Офисный компьютер MXP", 29_990, 0, "Intel Core i3", 8, "1 ТБ"));
     }
 }
