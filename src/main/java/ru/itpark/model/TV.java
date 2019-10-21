246c8ad855a05b8c0afbfa5973cc07dd81232735
bertRC <sithalbert@ya.ru>
2019-10-22 01:04:22 +0300

    Создан класс TV

---
+++ src/main/java/ru/itpark/model/TV.java
@@ -0,0 +1,37 @@
+package ru.itpark.model;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+public class TV extends AbstractProduct {
+    private int diagonal;
+    private String color;
+    private String hdFormat;
+
+    public TV(long id, String name, int price, int rating, int diagonal, String color, String hdFormat) {
+        super(id, name, price, rating);
+        this.diagonal = diagonal;
+        this.color = color;
+        this.hdFormat = hdFormat;
+    }
+
+    @Override
+    public String toString() {
+        return "TV{" +
+                "id=" + super.getId() +
+                ", name='" + super.getName() + '\'' +
+                ", price=" + super.getPrice() +
+                ", rating=" + super.getRating() +
+                ", diagonal=" + diagonal +
+                ", color='" + color + '\'' +
+                ", hdFormat='" + hdFormat + '\'' +
+                '}';
+    }
+
+    @Override
+    public boolean match(String productType) {
+        return productType.toLowerCase().equals("tv");
+    }
+}
