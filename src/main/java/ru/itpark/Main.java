0adf224dd9f54091bcf8ca591c84ffc3d6befbc8
bertRC <sithalbert@ya.ru>
2019-10-21 21:16:41 +0300

    Проверям работу метода add, добавляющего коллекцию

--- src/main/java/ru/itpark/Main.java
+++ src/main/java/ru/itpark/Main.java
@@ -1,15 +1,21 @@
 package ru.itpark;
 
+import ru.itpark.model.AbstractProduct;
 import ru.itpark.model.Computer;
 import ru.itpark.model.Smartphone;
 import ru.itpark.repository.ProductRepository;
 import ru.itpark.service.ProductService;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class Main {
     public static void main(String[] args) {
         final ProductService service = new ProductService(new ProductRepository());
-        service.add(new Smartphone("Смартфон Redmi Note 7", 13_990, 4.0, "blue", 6.3, 32));
-        service.add(new Computer("Офисный компьютер MXP", 29_990, 0, "Intel Core i3", 8, "1 ТБ"));
+        Collection<AbstractProduct> collection = new LinkedList<>();
+        collection.add(new Smartphone("Смартфон Redmi Note 7", 13_990, 4.0, "blue", 6.3, 32));
+        collection.add(new Computer("Офисный компьютер MXP", 29_990, 0, "Intel Core i3", 8, "1 ТБ"));
+        service.add(collection);
 
         System.out.println(service.getRepository().getAll());
     }
