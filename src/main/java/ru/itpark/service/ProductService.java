628e171f8a7c30a4ef4208a67799c2b7d5344e4d
bertRC <sithalbert@ya.ru>
2019-10-21 20:15:13 +0300

    Добавлен метод add

--- src/main/java/ru/itpark/service/ProductService.java
+++ src/main/java/ru/itpark/service/ProductService.java
@@ -1,5 +1,6 @@
 package ru.itpark.service;
 
+import ru.itpark.model.AbstractProduct;
 import ru.itpark.repository.ProductRepository;
 
 public class ProductService {
@@ -7,5 +8,18 @@ public class ProductService {
 
     public ProductService(ProductRepository repository) {
         this.repository = repository;
+    }
+
+    public void add(AbstractProduct item) {
+        if (item.getId() != 0) {
+            throw new IllegalArgumentException("id must be 0");
+        }
+        if (item.getPrice() <= 0) {
+            throw new IllegalArgumentException("price must be greater than 0");
+        }
+        if (item.getRating() < 0 || item.getRating() > 5) {
+            throw new IllegalArgumentException("rating must be in the range 0 through 5");
+        }
+        repository.save(item);
     }
 }
