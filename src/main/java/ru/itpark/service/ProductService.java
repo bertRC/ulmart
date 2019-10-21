1cf11f7f4647a9a2333e25e9fb8db6c86320ac48
bertRC <sithalbert@ya.ru>
2019-10-21 20:52:32 +0300

    Добавлен метод add, добавляющий коллекцию элементов

--- src/main/java/ru/itpark/service/ProductService.java
+++ src/main/java/ru/itpark/service/ProductService.java
@@ -3,6 +3,8 @@ package ru.itpark.service;
 import ru.itpark.model.AbstractProduct;
 import ru.itpark.repository.ProductRepository;
 
+import java.util.Collection;
+
 public class ProductService {
     private final ProductRepository repository;
 
@@ -25,5 +27,20 @@ public class ProductService {
             throw new IllegalArgumentException("rating must be in the range 0 through 5");
         }
         repository.save(item);
+    }
+
+    public void add(Collection<AbstractProduct> items) {
+        for (AbstractProduct item : items) {
+            if (item.getId() != 0) {
+                throw new IllegalArgumentException("id must be 0");
+            }
+            if (item.getPrice() <= 0) {
+                throw new IllegalArgumentException("price must be greater than 0");
+            }
+            if (item.getRating() < 0 || item.getRating() > 5) {
+                throw new IllegalArgumentException("rating must be in the range 0 through 5");
+            }
+            repository.save(item);
+        }
     }
 }
